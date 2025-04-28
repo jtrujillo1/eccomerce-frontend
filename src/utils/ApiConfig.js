2a8b72d31fe1c8105111229dc9edf840157f22b8
jtrujillo1 <johnelias1.developer@gmail.com>
2025-04-27 22:26:32 -0500

    fix: url

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,5 +1,5 @@
 export const ApiConfig = {
-  API_BASE_URL: "http://3.80.65.141:3000",
+  API_BASE_URL: "https://hammerhead-app-qb357.ondigitalocean.app",
   API_WOMPI_URL: "https://api-sandbox.co.uat.wompi.dev/v1/transactions/",
   PRV_KEY: "prv_stagtest_5i0ZGIGiFcDQifYsXxvsny7Y37tKqFWg",
 };
